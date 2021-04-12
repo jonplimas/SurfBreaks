91b91de7c32e9d88249f192f2cf2e19f49e6fc83
jonplimas <42780131+jonplimas@users.noreply.github.com>
2021-04-12 10:15:16 -0700

    auth.php

--- auth.php
+++ auth.php
@@ -5,11 +5,11 @@ if (isset($_POST['username1']) && isset($_POST['password1'])) {
     $password = trim($_POST['password1']);
 
     if (empty($email)) {
-        header("Location: login.php?error=Email is required")
-    } else if(empty($password)) {
-        header("Location: login.php?error=Password is required")
+        header("Location: login.php?error=Email is required");
+    } else if (empty($password)) {
+        header("Location: login.php?error=Password is required");
     } else {
         echo "Good!";
     }
-
 }
+?>
