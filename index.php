5978165934ec3e734351f53cc7b2273ae92adec4
jonplimas <42780131+jonplimas@users.noreply.github.com>
2021-04-18 02:43:26 -0700

    displays all

--- index.php
+++ index.php
@@ -71,47 +71,23 @@
 
 
             <div class="gallery">
-                <!-- <a href="">eatmybutt</a> -->
-                <!-- WIDGET TEST -->
-                <link href="//www.surf-forecast.com/stylesheets/widget.css" media="screen" rel="stylesheet" type="text/css" />
-                <div class="wf-width-cont surf-fc-widget">
-                    <div class="widget-container">
-                        <div class="external-cont">
-                            <iframe class="surf-fc-i" allowtransparency="true" src="//www.surf-forecast.com/breaks/Church/forecasts/widget/a" scrolling="no" frameborder="0" marginwidth="0" marginheight="0"></iframe>
-                            <div class="footer">
-                                <a class="logo" href="//www.surf-forecast.com/"><img src="//www.surf-forecast.com/images/widget.png" alt="Widget" width="1" height="1" /></a>
-                                    <div class="about" id="cmt">
-                                        More <a href="//www.surf-forecast.com/breaks/Church">Detailed Surf Conditions &amp; Webcams for&nbsp;Church</a><nobr>at&nbsp;<a href="//www.surf-forecast.com/">surf-forecast.com</a></nobr>.
-                                    </div>
-                            </div>
-                        </div>
-                     </div>
-                </div>
-
                 <?php
                     echo "<br>";
 
                     $report = $conn->query($_SESSION['sort_by'])->fetchAll();
                     $filter = $conn->query("SELECT * FROM surf_gallery")->fetchAll();
-
-                    print_r($filter);
-
-                    $tab = [];
+                    
                     foreach($filter as $i) {
                         if($i['gallery_owner'] === $userid){
-                            array_push($tab, $i['forecast_name']);
+                            foreach($report as $j) {
+                                if($j['surf_name'] === $i['forecast_name']) {
+                                    print_r($j['surf_report']);
+                                    echo "<br>";
+                                }
+                            }
                         }
                     }
-
-                    foreach($report as $i) {
-                        if($i['surf_name'] == $tab) {
-                            print_r($i['surf_report']);
-                        }
-                    }
-                    
                 ?>
-
-                
                 </div>
 
             </div>
